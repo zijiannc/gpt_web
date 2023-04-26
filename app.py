36260a598893fca0e7c31f5eec65bcdfc18b0b55
Neal C <zijiannc@gmail.com>
2023-04-26 10:16:22 -0700

    第一个version

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ import openai
 app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+# 创建一个字典来存储每个用户的消息历史
+user_histories = {}
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -14,27 +17,40 @@ def index():
 def chat():
     # 从请求中获取用户输入的消息
     user_message = request.json['message']
+    
+    # 获取用户的IP地址
+    user_ip = request.remote_addr
 
-    # 构造与API交互所需的消息列表
-    messages = [
-        {"role": "system", "content": "你好，我是一个聊天机器人。请问有什么问题我可以帮助您解答吗？"},
-        {"role": "user", "content": user_message}
-    ]
+    # 如果用户历史记录中没有该IP地址，创建一个空列表
+    if user_ip not in user_histories:
+        user_histories[user_ip] = [{"role": "system", "content": "你好,我是您的私人顾问,我可以回答任何问题."}] # 机器人的欢迎语
+    
+    # 向历史记录中添加用户的消息
+    user_histories[user_ip].append({"role": "user", "content": user_message})
 
-    # 调用OpenAI API来获取回复    
+    # 调用OpenAI API来获取回复
     completion = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            temperature=0.8,
-            messages=messages
-        )
+        model="gpt-3.5-turbo",
+        temperature=0.8,
+        messages=user_histories[user_ip]
+    )
 
     # 从API响应中提取回复文本
     response = completion.choices[0].message.content
-    #response = completions.choices[0].message['content'].strip()
+
+    # 向历史记录中添加机器人的回复
+    user_histories[user_ip].append({"role": "assistant", "content": response})
 
     # 将回复作为JSON格式返回
     return jsonify({"response": response})
 
+@app.route('/clear_history', methods=['POST'])
+def clear_history():
+    user_ip = request.remote_addr
+    if user_ip in user_histories:
+        user_histories.pop(user_ip)
+    return "OK"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    #app.run(debug=True)
+    app.run(host='0.0.0.0', port=5001, debug=True)
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,52 +1,74 @@
 // 页面加载完成后，绑定事件监听器
-$(document).ready(function() {
+$(document).ready(function () {
     $("#sendButton").on("click", sendMessage);
-    $("#messageInput").on("keypress", function(event) {
-      if (event.keyCode === 13) {
-        event.preventDefault();
-        sendMessage();
-      }
+    $("#messageInput").on("keypress", function (event) {
+        if (event.keyCode === 13) {
+            event.preventDefault();
+            sendMessage();
+        }
     });
-  });
-  
-  // 发送消息函数
-  function sendMessage() {
+
+    // 在页面加载时清除历史记录
+    clearHistory();
+});
+
+// 发送消息函数
+function sendMessage() {
     const message = $("#messageInput").val();
     if (!message) return;
-  
+
     displayUserMessage(message);
     $("#messageInput").val("");
-  
+
     // 使用Ajax向后端发送请求
     $.ajax({
-      url: "/chat",
-      type: "POST",
-      data: JSON.stringify({ message: message }),
-      contentType: "application/json",
-      success: function(response) {
-        const botResponse = response.response;
-        displayBotMessage(botResponse);
-      },
-      error: function(err) {
-        console.log(err);
-      },
+        url: "/chat",
+        type: "POST",
+        data: JSON.stringify({ message: message }),
+        contentType: "application/json",
+        success: function (response) {
+            const botResponse = response.response;
+            displayBotMessage(botResponse);
+        },
+        error: function (err) {
+            console.log(err);
+        },
     });
-  }
-  
-  // 显示用户消息
-  function displayUserMessage(message) {
+}
+
+// 显示用户消息
+function displayUserMessage(message) {
     $("#chatArea").append(`<div class="message"><span class="user">你: </span>${message}</div>`);
     scrollToBottom();
-  }
-  
-  // 显示机器人消息
-  function displayBotMessage(message) {
+
+    // 将聊天记录保存到localStorage
+    localStorage.setItem("chatHistory", $("#chatArea").html());
+}
+
+// 显示机器人消息
+function displayBotMessage(message) {
     $("#chatArea").append(`<div class="message"><span class="bot">机器人: </span>${message}</div>`);
     scrollToBottom();
-  }
-  
-  // 将聊天区域滚动到底部
-  function scrollToBottom() {
+
+    // 更新localStorage中的聊天记录
+    localStorage.setItem("chatHistory", $("#chatArea").html());
+}
+
+// 将聊天区域滚动到底部
+function scrollToBottom() {
     $("#chatArea").scrollTop($("#chatArea")[0].scrollHeight);
-  }
-  
+}
+
+// 清除历史记录函数
+function clearHistory() {
+    $.ajax({
+        url: "/clear_history",
+        type: "POST",
+        success: function() {
+            console.log("History cleared.");
+        },
+        error: function(err) {
+            console.log(err);
+        },
+    });
+}
